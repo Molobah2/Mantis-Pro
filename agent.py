820ca5a9e3cba976a6992818f392be6fbc4dfc33
Molobah2 <smolobah21@gmail.com>
2026-05-31 20:00:16 +0100

    add bunny leaderboard tool

--- agent.py
+++ agent.py
@@ -66,6 +66,15 @@ def bunny_hub():
     from flask import Response
     return Response(html, mimetype="text/html")
 
+@app.route("/bunny/leaderboard")
+def bunny_leaderboard():
+    import os
+    path = os.path.join(os.path.dirname(__file__), "bunny_leaderboard.html")
+    with open(path, "r", encoding="utf-8") as f:
+        html = f.read()
+    from flask import Response
+    return Response(html, mimetype="text/html")
+
 register_bunny_routes(app)
 
 @app.route("/moody/woke")
